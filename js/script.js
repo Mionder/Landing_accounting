187b6ed4b89f49ebfb8ae7ff2ca809906159dc6b
Mionder <49869397+Mionder@users.noreply.github.com>
2020-09-17 16:29:07 +0300

    footer media

--- js/script.js
+++ js/script.js
@@ -29,6 +29,12 @@ $(document).ready(function(){
           top = $(id).offset().top;
       $('body,html').animate({scrollTop: top}, 1500);
   });
+  $("#footer__menu").on("click","a", function (event){
+    event.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body, html').animate({scrollTop: top}, 1500);
+  });
 });
 
 var btn = $('#button');
